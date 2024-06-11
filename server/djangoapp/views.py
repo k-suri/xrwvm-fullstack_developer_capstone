05aa44a35bc472c9d3137d22181c673da535c60e
“k-suri” <khushimuskaan1999@gmail.com>
2024-06-11 01:10:22 -0400

    added review and post review

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -96,6 +96,7 @@ def get_dealer_reviews(request, dealer_id):
     if(dealer_id):
         endpoint = "/fetchReviews/dealer/"+str(dealer_id)
         reviews = get_request(endpoint)
+        print(reviews)
         for review_detail in reviews:
             response = analyze_review_sentiments(review_detail['review'])
             print(response)
